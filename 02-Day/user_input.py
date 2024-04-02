466409865664dc82eb5168700ad19adc373bb184
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-02 09:30:00 +0530

    Day 2: User input variables

--- 02-Day/user_input.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-
-print("Hello", name)
-print("You are", age, "years old")
-print("You will be", int(age) + 1, "years old next year")
