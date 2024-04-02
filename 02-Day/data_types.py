f09d45a34eeec3c209fe0f4437ac9860c2ee05bb
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-02 09:10:00 +0530

    Day 2: Learned Python data types

---
+++ 02-Day/data_types.py
@@ -0,0 +1,10 @@
+name = "Ajay"      # string
+age = 17           # integer
+height = 5.6       # float
+is_student = True  # boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(name, age, height, is_student)
