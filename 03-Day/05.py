163066d4eaa5e607602adf6239d4290732bb6610
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-03 09:40:00 +0530

    Day 3: Operator tasks done

---
+++ 03-Day/05.py
@@ -0,0 +1,16 @@
+# Small Task (Operators Mix)
+
+
+# Task: Calculate simple interest
+
+p = 1000
+r = 10
+t = 2
+
+si = (p * r * t) / 100
+print("Simple Interest:", si)
+
+# Task: Check if a number is positive
+
+num = -5
+print("Is positive:", num > 0)
