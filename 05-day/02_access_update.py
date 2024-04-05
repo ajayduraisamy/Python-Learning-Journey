08abf44938cf7279fe16f1dde56a3cf2d25e1d05
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-05 10:02:00 +0530

    Day5_02: access + update dict (tink)

---
+++ 05-day/02_access_update.py
@@ -0,0 +1,11 @@
+# 02_access_update.py
+# Accessing and updating dictionary
+
+car = {"brand": "BMW", "year": 2020}
+
+print("Brand:", car["brand"])
+
+car["year"] = 2024
+car["color"] = "black"
+
+print(car)
