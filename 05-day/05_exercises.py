98f6d7d6851db4ae6e8d4090276b89f64c6b8934
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-05 10:05:00 +0530

    Day5_05: dictionary exercises (tink)

---
+++ 05-day/05_exercises.py
@@ -0,0 +1,9 @@
+# 05_exercises.py
+# Small dictionary exercises
+
+marks = {"math": 80, "science": 75, "english": 90}
+total = sum(marks.values())
+avg = total / len(marks)
+
+print("Total:", total)
+print("Average:", avg)
