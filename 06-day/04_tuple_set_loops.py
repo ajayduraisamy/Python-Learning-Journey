82a8ab9036555d8a56a46774b6d5ff9b9ea04cfa
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-06 10:04:00 +0530

    Day6_04: loops on tuple + set (tink)

---
+++ 06-day/04_tuple_set_loops.py
@@ -0,0 +1,12 @@
+# 04_tuple_set_loops.py
+# Loop through tuples and sets
+
+nums = (1, 2, 3)
+print("Tuple loop:")
+for n in nums:
+    print(n)
+
+langs = {"python", "java", "c++"}
+print("Set loop:")
+for l in langs:
+    print(l)
