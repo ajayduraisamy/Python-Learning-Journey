45a14c95c73a9f4d10da773f2953389d37576239
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-06 10:01:00 +0530

    Day6_01: list append + insert (tink)

---
+++ 06-day/01_list_append_insert.py
@@ -0,0 +1,8 @@
+# 01_list_append_insert.py
+# Day 6 - List append + insert
+
+numbers = [10, 20, 30]
+numbers.append(40)    # add at end
+numbers.insert(1, 15) # insert at index
+
+print("Updated list:", numbers)
