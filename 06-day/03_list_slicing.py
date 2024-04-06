4936117c1cafb82f1dc0a1011d8bf54b5c9e9aff
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-06 10:03:00 +0530

    Day6_03: list slicing examples (tink)

---
+++ 06-day/03_list_slicing.py
@@ -0,0 +1,8 @@
+# 03_list_slicing.py
+# slicing examples
+
+langs = ["python", "java", "c++", "javascript", "go"]
+
+print("First 3:", langs[:3])
+print("Middle:", langs[1:4])
+print("Last 2:", langs[-2:])
