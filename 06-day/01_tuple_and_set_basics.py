20614d70d74ad8b93f9acd9ca9ccddc7f0e975aa
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-06 10:01:00 +0530

    Day6_01: tuple + set basics (tink)

---
+++ 06-day/01_tuple_and_set_basics.py
@@ -0,0 +1,10 @@
+# 01_tuple_and_set_basics.py
+# Day 6 - Tuples + Sets basics
+
+# Tuple basics
+numbers = (10, 20, 30, 40)
+print("Tuple:", numbers)
+
+# Set basics
+items = {1, 2, 3, 3, 4}
+print("Set (duplicates removed):", items)
