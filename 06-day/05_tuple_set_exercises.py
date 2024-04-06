63dab5b676ababb776ec8358cb82d47edeab7d21
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-06 10:05:00 +0530

    Day6_05: tuple + set exercises (tink)

---
+++ 06-day/05_tuple_set_exercises.py
@@ -0,0 +1,11 @@
+# 05_tuple_set_exercises.py
+# Combined tuple + set exercises
+
+fruits = ("apple", "banana", "orange", "mango")
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+unique_nums = {1, 2, 3}
+unique_nums.add(4)
+unique_nums.update({5, 6})
+print("Updated set:", unique_nums)
