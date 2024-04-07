3b2c0582f8d146bb966e0a0cbfbbcaf1e473795f
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-07 10:05:00 +0530

    Day7_05: condition exercises (tink)

---
+++ 07-day/05_condition_exercises.py
@@ -0,0 +1,19 @@
+# 05_condition_exercises.py
+# mini exercises
+
+num = 7
+
+# Even or odd
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Positive / Negative / Zero
+value = -3
+if value > 0:
+    print("Positive")
+elif value < 0:
+    print("Negative")
+else:
+    print("Zero")
