53620ff3caa8c1a2f5c2e2779d35501aef199789
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-07 10:03:00 +0530

    Day7_03: if elif else ladder (tink)

---
+++ 07-day/03_if_elif_else.py
@@ -0,0 +1,13 @@
+# 03_if_elif_else.py
+# if, elif, else ladder
+
+score = 85
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 50:
+    print("Grade C")
+else:
+    print("Grade F")
