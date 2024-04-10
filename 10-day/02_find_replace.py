cd9325415ac16b040b8542de67b3bf109c7e46ed
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-10 10:02:00 +0530

    Day10_02: string find + replace (tink)

---
+++ 10-day/02_find_replace.py
@@ -0,0 +1,9 @@
+# 02_find_replace.py
+# find() and replace()
+
+msg = "python is awesome"
+
+print("Index of 'awesome':", msg.find("awesome"))
+new_msg = msg.replace("awesome", "powerful")
+
+print("Replaced:", new_msg)
