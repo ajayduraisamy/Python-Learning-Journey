5293a0ee78fbe7b3b9bcba39169874913016aa71
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-11 10:02:00 +0530

    Day11_02: list reverse examples (tink)

---
+++ 11-day/02_list_reverse.py
@@ -0,0 +1,9 @@
+# 02_list_reverse.py
+# reverse() method
+
+names = ["Ajay", "Kumar", "Suri", "Raj"]
+
+names.reverse()
+print("Reversed list:", names)
+
+print("Reverse using slicing:", names[::-1])
