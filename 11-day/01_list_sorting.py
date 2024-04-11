45199fc67ca585609d26dfe816a8a07af7399651
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-11 10:01:00 +0530

    Day11_01: list sorting examples (tink)

---
+++ 11-day/01_list_sorting.py
@@ -0,0 +1,12 @@
+# 01_list_sorting.py
+# Sorting lists
+
+nums = [5, 2, 9, 1, 7]
+
+nums.sort()
+print("Sorted ascending:", nums)
+
+nums.sort(reverse=True)
+print("Sorted descending:", nums)
+
+print("Using sorted():", sorted(nums))
