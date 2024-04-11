401ac508bfca08d289e2d7e26a473c78a29f5630
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-11 10:04:00 +0530

    Day11_04: nested lists example (tink)

---
+++ 11-day/04_nested_lists.py
@@ -0,0 +1,14 @@
+# 04_nested_lists.py
+# Nested lists example
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("Matrix:")
+for row in matrix:
+    print(row)
+
+print("Element [1][2]:", matrix[1][2])
