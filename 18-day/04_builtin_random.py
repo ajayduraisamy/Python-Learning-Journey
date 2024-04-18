01f5092ff78872446e7f8eedd187ce655cba6631
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-18 10:04:00 +0530

    Day18_04: builtin random module examples (tink)

---
+++ 18-day/04_builtin_random.py
@@ -0,0 +1,7 @@
+# 04_builtin_random.py
+# Using random module
+
+import random
+
+print("Random number (1-100):", random.randint(1, 100))
+print("Random choice:", random.choice(["Ajay", "Kumar", "Suri", "Raj"]))
