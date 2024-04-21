ae1983d279d709ef09662d753e4ba090bb3a634f
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-21 10:04:00 +0530

    Day21_04: extract datetime parts (tink)

---
+++ 21-day/04_extract_parts.py
@@ -0,0 +1,12 @@
+# 04_extract_parts.py
+# Extracting specific values from datetime
+
+from datetime import datetime
+
+now = datetime.now()
+
+print("Year:", now.year)
+print("Month:", now.month)
+print("Day:", now.day)
+print("Hour:", now.hour)
+print("Minute:", now.minute)
