4adf2df92da7dc343f1aadfc3bc97c458db5b5a2
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-24 10:05:00 +0530

    Day24_05: math exercises (tink)

---
+++ 24-day/05_math_exercises.py
@@ -0,0 +1,18 @@
+# 05_math_exercises.py
+# Practice problems
+
+import math
+
+# 1. Area of circle
+r = 7
+area = math.pi * r * r
+print("Area of circle:", area)
+
+# 2. Hypotenuse using Pythagoras
+a, b = 3, 4
+hyp = math.sqrt(a*a + b*b)
+print("Hypotenuse:", hyp)
+
+# 3. Convert degrees to radians
+deg = 180
+print("Radians:", math.radians(deg))
