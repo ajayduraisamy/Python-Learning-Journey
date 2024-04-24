bba282e8c50a83d2a0b8f60b838470094d653680
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-24 10:02:00 +0530

    Day24_02: math factorial, pow, gcd (tink)

---
+++ 24-day/02_math_advanced.py
@@ -0,0 +1,8 @@
+# 02_math_advanced.py
+# factorial, power, gcd
+
+import math
+
+print("Factorial 5:", math.factorial(5))
+print("Power 2^5:", math.pow(2, 5))
+print("GCD (48, 18):", math.gcd(48, 18))
