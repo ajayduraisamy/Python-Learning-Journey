5aac6a49bbc8d369abb4c898fb10915de27147b1
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-04-30 10:01:00 +0530

    Day30_01: basic logging info+warning (tink)

---
+++ 30-day/01_basic_logging.py
@@ -0,0 +1,8 @@
+# 01_basic_logging.py
+# Basic logging example
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.info("Program started!")
+logging.warning("This is a warning message.")
