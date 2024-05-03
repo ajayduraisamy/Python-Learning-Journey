1078ef2770a2f8dcd405901a2aace22136f3bbef
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-05-03 10:01:00 +0530

    Day33_01: basic decorator example (tink)

---
+++ 33-day/01_basic_decorator.py
@@ -0,0 +1,15 @@
+# 01_basic_decorator.py
+# Basic decorator example
+
+def my_decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello Ajay!")
+
+say_hello()
