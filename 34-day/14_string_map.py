91877299f1ca7c6ee94c33cc6d57d1be67c4e617
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-05-04 10:14:00 +0530

    Day34_14: uppercase conversion comprehension (tink)

---
+++ 34-day/14_string_map.py
@@ -0,0 +1,6 @@
+# 14_string_map.py
+# Convert list of strings to uppercase with comprehension
+
+names = ["ajay", "kumar", "suri"]
+upper_names = [name.upper() for name in names]
+print(upper_names)
