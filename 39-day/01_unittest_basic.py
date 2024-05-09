a259ceb34a9510781c28eb0f26425028394ee577
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-05-09 10:01:00 +0530

    Day39_01: basic unittest example (tink)

---
+++ 39-day/01_unittest_basic.py
@@ -0,0 +1,14 @@
+# 01_unittest_basic.py
+# Basic unittest test case
+
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
