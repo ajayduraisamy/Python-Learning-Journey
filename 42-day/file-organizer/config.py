d9057174bcf4a6b5f07b3bbcb033c055c7a76a19
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-05-12 10:05:00 +0530

    Day42_05: add extension mapping config (tink)

---
+++ 42-day/file-organizer/config.py
@@ -0,0 +1,9 @@
+# config.py
+# File extension mapping
+
+EXTENSION_MAP = {
+    "images": ["jpg", "jpeg", "png", "gif"],
+    "videos": ["mp4", "mkv", "mov"],
+    "documents": ["pdf", "docx", "txt"],
+    "compressed": ["zip", "rar", "7z"],
+}
