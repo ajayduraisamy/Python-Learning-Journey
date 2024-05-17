d8b95e5addbe80c2c459f905b0344d41889bb5b5
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-05-17 10:08:00 +0530

    Day47_08: add training test (tink)

---
+++ 47-day/logistic-regression/08_test_model.py
@@ -0,0 +1,10 @@
+# 08_test_model.py
+# Basic test for model training
+
+from 01_dataset import get_train_test
+from 02_model import train_model
+
+def test_training():
+    X_train, X_test, y_train, y_test = get_train_test()
+    model = train_model(X_train, y_train)
+    assert hasattr(model, "predict")
